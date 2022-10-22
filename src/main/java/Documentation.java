f166d0357dea6e64d8866286cf80c8b47b01db6d
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 14:12:58 +0300

    Updated documentation

--- src/main/java/Documentation.java
+++ src/main/java/Documentation.java
@@ -1,7 +1,13 @@
 public class Documentation {
-    /**
+    /** Environment is Inellij Community
+     * JDK version is : JDK 11      ... Download it from :  https://www.techspot.com/downloads/5553-java-jdk.html
      * 1. Youtube link : https://www.youtube.com/watch?v=a7T2MU2l-es
      * 2. To add workflow, In github click Actions > then select Gradle, the create gradle.yml
      *    Then git pull
+     *
+     *3. To solve the Error "Error: Error: Gradle script '/home/runner/work/Github-Actions-Gradle/Github-Actions-Gradle/gradlew' is not executable."
+     *   Run the following Command :
+     *             git update-index --chmod=+x gradlew
+     *
      * **/
 }
