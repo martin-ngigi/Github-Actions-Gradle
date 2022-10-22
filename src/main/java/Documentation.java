12169a9f017dfe754bf5996976a406233b473011
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 14:04:23 +0300

    Enable Gradle caching

--- src/main/java/Documentation.java
+++ src/main/java/Documentation.java
@@ -1,5 +1,7 @@
 public class Documentation {
     /**
      * 1. Youtube link : https://www.youtube.com/watch?v=a7T2MU2l-es
+     * 2. To add workflow, In github click Actions > then select Gradle, the create gradle.yml
+     *    Then git pull
      * **/
 }
